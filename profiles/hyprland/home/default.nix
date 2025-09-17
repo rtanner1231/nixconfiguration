24411fc235bf184a7b7c3b464a9300e73d0a901d
Richard Tanner <rtanner@myron.com>
2025-09-16 22:51:57 -0400

    Create homeConfig utility function

--- profiles/hyprland/home/default.nix
+++ profiles/hyprland/home/default.nix
@@ -1,26 +1,16 @@
 {
-  pkgs,
   inputs,
   ...
 }:
 let
-  username = "rick";
+  userModule = import ../../../util/homeConfig.nix;
 in
 {
-  imports = [ inputs.home-manager.nixosModules.home-manager ];
-  home-manager = {
-    useUserPackages = true;
-    useGlobalPkgs = true;
-    extraSpecialArgs = { inherit inputs; };
-    users.rick = {
-      imports = [ ./home.nix ];
-      home.username = "${username}";
-      home.homeDirectory = "/home/${username}";
-      home.stateVersion = "24.05";
-      programs.home-manager.enable = true;
-    };
-    backupFileExtension = "backup";
-  };
-
-  nix.settings.allowed-users = [ "${username}" ];
+  imports = [
+    (userModule {
+      inherit inputs;
+      username = "rick";
+      homeFile = ./home.nix;
+    })
+  ];
 }
---
+++ util/homeConfig.nix
@@ -0,0 +1,30 @@
+{
+  inputs,
+  username,
+  homeFile,
+}:
+
+{
+  imports = [ inputs.home-manager.nixosModules.home-manager ];
+
+  home-manager = {
+    useUserPackages = true;
+    useGlobalPkgs = true;
+    backupFileExtension = "backup";
+
+    extraSpecialArgs = { inherit inputs; };
+
+    users."${username}" = {
+      imports = [ homeFile ];
+      home = {
+        inherit username;
+        homeDirectory = "/home/${username}";
+        stateVersion = "24.05";
+      };
+      programs.home-manager.enable = true;
+    };
+  };
+
+  # Also use the 'username' argument here
+  nix.settings.allowed-users = [ username ];
+}
