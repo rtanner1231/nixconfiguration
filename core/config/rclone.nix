d300496c62365a520d544d30116d3b05ab77275a
Richard Tanner <rtanner@myron.com>
2025-10-13 08:35:11 -0400

    Add rclone

--- core/config/rclone.nix
+++ core/config/rclone.nix
@@ -10,37 +10,46 @@ let
     "/.cache/**"
     "/.zen/**"
     "/.config/slack/**"
+    "/.thunderbird/**"
+    "/.config/cosmic/**"
   ];
 in
 {
   environment.systemPackages = with pkgs; [
     rclone
   ];
-  # Define the systemd service unit
-  systemd.services."rclone-backup" = {
-    description = "Rclone backup for user ${profile.user}";
-    after = [ "network-online.target" ];
-    wants = [ "network-online.target" ];
+
+  systemd.user.services."rclone-backup" = {
+    description = "Rclone backup service";
+    after = [
+      "network-online.target"
+      "systemd-resolved.service"
+    ];
+    wants = [
+      "network-online.target"
+      "systemd-resolved.service"
+    ];
     # Specifies the packages needed in the environment PATH for the command
     path = [ pkgs.rclone ];
     # The actual command to run, using the injected variables
+    # Sleep for 30 seconds to give time for DNS to resolve
     script = ''
       set -eu
+      sleep 30
       rclone sync "/home/${profile.user}" "GDrive:${profile.name}" \
         ${pkgs.lib.concatMapStringsSep " " (p: "--exclude '${p}'") excludePaths} \
         --log-level INFO
     '';
     # Service configuration details
     serviceConfig = {
-      Type = "oneshot";
-      # The service runs a command once and then exits
-      User = profile.user;
-      Group = "users";
+      Type = "simple";
+      Restart = "on-failure";
+      RestartSec = "5m";
     };
   };
 
   # Define the systemd timer unit to trigger the service
-  systemd.timers."rclone-backup" = {
+  systemd.user.timers."rclone-backup" = {
     description = "Daily timer for rclone backup";
     wantedBy = [ "timers.target" ]; # Ensures the timer is started on boot
     timerConfig = {
