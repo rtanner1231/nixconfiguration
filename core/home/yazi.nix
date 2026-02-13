5ddd59536bb52aec61ebe2d2a52bbd46c2b2149d
Richard Tanner <rtanner@myron.com>
2026-02-12 19:35:10 -0500

    Yazi opener updates

--- core/home/yazi.nix
+++ core/home/yazi.nix
@@ -2,6 +2,43 @@
 {
   programs.yazi = {
     enable = true;
+    settings = {
+      opener = {
+        kate = [
+          {
+            run = "kate \"$@\"";
+            orphan = true;
+            desc = "Kate";
+          }
+        ];
+        calc = [
+          {
+            run = "libreoffice --calc \"$@\"";
+            orphan = true;
+            desc = "LibreOffice Calc";
+          }
+        ];
+      };
+      open = {
+        rules = [
+          {
+            mime = "text/*";
+            use = [
+              "edit"
+              "kate"
+              "calc"
+            ];
+          }
+          {
+            url = "*.csv";
+            use = [
+              "calc"
+              "kate"
+            ];
+          }
+        ];
+      };
+    };
     keymap = {
       mgr.prepend_keymap = [
         {
@@ -11,6 +48,11 @@
           ];
           run = "shell 'zoxide add \"$0\"'";
           desc = "Add directory to zoxide";
+        }
+        {
+          on = [ "<Enter>" ];
+          run = "shell 'if [ -d \"$1/.git\" ]; then nohup meld \"$1\" > /dev/null 2>&1 & else ya emit enter; fi'";
+          desc = "Open git repo in Meld, otherwise enter directory";
         }
       ];
     };
